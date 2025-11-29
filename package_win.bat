e9ee11b78f4edf80f0e72d87efc1ebc2ea31d48a
gabethebabe <gabrielgad911@yahoo.com>
2025-11-29 08:04:13 -0500

    Add lib_steamnetworkingsockets to Windows build matrix
    This enables building the steamnetworkingsockets library which is needed
    for games like The Riftbreaker that use GameNetworkingSockets.dll separately
    from steam_api64.dll.

--- package_win.bat
+++ package_win.bat
@@ -73,6 +73,10 @@ if exist "%TARGET_DIR%\tools\generate_interfaces\" (
 if exist "%TARGET_DIR%\tools\lobby_connect\" (
   copy /y "%ROOT%\post_build\README.lobby_connect.md" "%TARGET_DIR%\tools\lobby_connect\"
 )
+
+if exist "%TARGET_DIR%\steamnetworkingsockets\" (
+  echo:// steamnetworkingsockets library found, including in package
+)
 ::::::::::::::::::::::::::::::::::::::::::
 
 set "ACHIVE_DIR=%OUT_DIR%\%~1"
